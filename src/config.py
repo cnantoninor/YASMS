4f79a45e15f09bb22f1fb469c43179e390b691be
cnantoninor <antonino.rau@gmail.com>
2024-01-14 14:05:18 -0500

    first data exploration done in notebook

--- src/config.py
+++ src/config.py
@@ -11,23 +11,18 @@ load_dotenv()
 class Paths:
     root: Path = Path(__file__).parent
     data: Path = root / "data"
-    book: Path = (
-            data
-            / "Marcus_Aurelius_Antoninus_-_His_Meditations_concerning_himselfe/index.html"
-    )
     mrau_weblive_project1: Path = (
-            data
-            / "wl_mrproject/mention_1305_2024-01-11-174801.csv"
+        data / "wl_mrproject/mention_1305_2024-01-11-174801.csv"
     )
     mrau_weblive_project1_1k_mentions: Path = (
-            data
-            / "wl_mrproject/1k_mention_1305_2024-01-11-174801.csv"
+        data / "wl_mrproject/1k_mention_1305_2024-01-11-174801.csv"
     )
 
     mrau_weblive_project1_5k_mentions: Path = (
-            data
-            / "wl_mrproject/5k_mention_1305_2024-01-11-174801.csv"
+        data / "wl_mrproject/5k_mention_1305_2024-01-11-174801.csv"
     )
+
+    testnino1_classification_task: Path = data / "wl_classif_testnino1/TESTNINO1.csv"
 
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
