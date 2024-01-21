67c4b77842eecbf0f529f7a4e5facd4da0d89a8f
cnantoninor <antonino.rau@gmail.com>
2024-01-20 22:47:45 -0500

    more ludwig

--- notebook/binary-classification_stato_workflow/ludwig/experiment_parallel_ccn-word-encodings.sh
+++ notebook/binary-classification_stato_workflow/ludwig/experiment_parallel_ccn-word-encodings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# FILEPATH: /home/arau6/projects/wl-semsearch-poc/notebook/binary-classification_stato_workflow/ludwig/experiment_parallel_ccn-word-encodings.sh
 
 ludwig experiment \
-  --dataset ../../../src/data/wl_classif_testnino1/TESTNINO1_with_boolean_StatoWorkflow.csv  \
-  --config ./ludwig-parallel-cnn-word-encoding.yml \
+  --config ./parallel-cnn-word-encoding.yml \
+  --training_set ./data/TESTNINO1_boolean_StatoWorkflow_train.csv \
+  --test_set ./data/TESTNINO1_boolean_StatoWorkflow_test.csv \
   --experiment_name parallel-cnn-word-encoding-balanced-train
