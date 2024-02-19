a7a04258151142ee96fd2e95e86be040cc8e2a27
cnantoninor <antonino.rau@gmail.com>
2024-02-19 18:17:54 -0500

    fixed remote

--- remote/setup-aruba.sh
+++ remote/setup-aruba.sh
@@ -1,6 +1,5 @@
 sudo apt update -y
 sudo apt install -y software-properties-common
-sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.10
 sudo apt install -y python3-pip
 pip install -r requirements.txt
