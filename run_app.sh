314c7280e345c58f550601dd0f9ecc2c90387f75
cnantoninor <antonino.rau@gmail.com>
2024-02-25 20:00:37 -0500

    added coverage

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 cd src
 uvicorn app:app --reload > uvicorn.err.log 2> uvicorn.log &
--- ssh-wl-poc.sh
+++ ssh-wl-poc.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 ssh -i "remote/wl-poc.pem" ec2-user@ec2-52-45-34-4.compute-1.amazonaws.com
