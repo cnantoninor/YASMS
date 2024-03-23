91e14c098298673914235437a233c038b33891f4
cnantoninor <antonino.rau@gmail.com>
2024-03-23 15:13:43 -0400

    fixed coverage

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export PYTHONPATH="${PYTHONPATH}:src"
-pytest --cov=wl-ml-server --cov-report html
+pytest --cov=src --cov-report html
+/mnt/c/Program\ Files\ \(x86\)/Microsoft/Edge/Application/msedge.exe file:////wsl.localhost\\Ubuntu\\home\\arau6\\projects\\wl-semsearch-poc\\htmlcov\\index.html
